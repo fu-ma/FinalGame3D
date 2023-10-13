51e5246e56eeedf42662a544a5dfc8faaa2b3295
kairin_mup <k020g1208@g.neec.ac.jp>
2023-10-13 12:17:53 +0900

    アイテム用にいじったもの

---
+++ Assets/Script/ItemDataBase.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemDataBase : ScriptableObject
+{
+    public List<PocketItem> itemList = new List<PocketItem>();
+}
+
