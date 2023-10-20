edcb73fe5f8406c5868796442bdc718cdffb6144
fuuma1223 <k020g1170@g.neec.ac.jp>
2023-10-20 17:07:33 +0900

    実装したこと ・移動時のアニメーションをすべて入れ終えた

--- Assets/Script/playerSpriteMove.cs
+++ Assets/Script/playerSpriteMove.cs
@@ -22,14 +22,24 @@ public class playerSpriteMove : MonoBehaviour
         if (inputAction_.Player.MoveLeft.IsPressed())
         {
             anim.enabled = true;
-            anim.SetBool("move", true);
+            //anim.SetBool("move", true);
             anim.Play("playerLeftAnimation");
         }
         else if(inputAction_.Player.MoveDown.IsPressed())
         {
             anim.enabled = true;
-            anim.SetBool("move", true);
+            //anim.SetBool("move", true);
             anim.Play("playerFowordAnimation");
+        }
+        else if(inputAction_.Player.MoveRight.IsPressed())
+        {
+            anim.enabled = true;
+            anim.Play("playerRightAnimation");
+        }
+        else if(inputAction_.Player.MoveUp.IsPressed())
+        {
+            anim.enabled = true;
+            anim.Play("playerDownAnimation");
         }
         else
         {
