562f89ff94bd24958321f9bbbcf6f3c5f6ffba95
kairin_mup <k020g1208@g.neec.ac.jp>
2024-01-29 12:00:27 +0900

    出来てるかわからんけどリセット(これからデバッグする)

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public bool redButtonPushed;
+    public bool blueButtonPushed;
+    public bool greenButtonPushed;
+    public bool yellowButtonPushed;
+    public bool isReset;
+
+    private bool trueButtonPas;
+    private int buttonScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        redButtonPushed = false;
+        blueButtonPushed = false;
+        greenButtonPushed = false;
+        yellowButtonPushed = false;
+        trueButtonPas = false;
+        isReset = false;
+        buttonScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (redButtonPushed == true && blueButtonPushed == false &&
+            greenButtonPushed == false && yellowButtonPushed == false && buttonScore == 0)
+        {
+            buttonScore += 1;
+        }
+        if (redButtonPushed == true && blueButtonPushed == true &&
+            greenButtonPushed == false && yellowButtonPushed == false && buttonScore == 1)
+        {
+            buttonScore += 1;
+        }
+        if (redButtonPushed == true && blueButtonPushed == true &&
+            greenButtonPushed == true && yellowButtonPushed == false && buttonScore == 2)
+        {
+            buttonScore += 1;
+        }
+        if (redButtonPushed == true && blueButtonPushed == true &&
+           greenButtonPushed == true && yellowButtonPushed == false && buttonScore == 3)
+        {
+            buttonScore += 1;
+        }
+        if (buttonScore == 4)
+        {
+            trueButtonPas = true;
+        }
+        if (isReset == true)
+        {
+            redButtonPushed = false;
+            blueButtonPushed = false;
+            greenButtonPushed = false;
+            yellowButtonPushed = false;
+            trueButtonPas = false;
+            buttonScore = 0;
+            isReset = false;
+        }
+    }
+}
---
+++ Assets/Script/ResetRoom.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class ResetRoom : MonoBehaviour
+{
+    public bool gameStart;
+    public bool resetButtonPushed;
+    public bool isReset;
+    public GameObject soraRoom;
+    public GameObject hakariRoom;
+    private GameObject soraRoomFirst;
+    private GameObject hakariRoomFirst;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameStart = true;
+        isReset = false;
+        resetButtonPushed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameStart == true)
+        {
+            hakariRoomFirst = hakariRoom;
+            hakariRoomFirst = soraRoom;
+            gameStart = false;
+        }
+        if (resetButtonPushed == true)
+        {
+            isReset = true;
+        }
+        if (isReset == true)
+        {
+            soraRoom = soraRoomFirst;
+            hakariRoom = hakariRoomFirst;
+        }
+    }
+}
